a294b1d9614c84bd8b2e8268b4db47b68db9b2a1
sachin <sachinbhattarai55@gmail.com>
2020-05-07 22:06:24 +0545

    re-stated

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ import {hot} from 'react-hot-loader/root';
 import React from 'react';
 import { jsx } from 'theme-ui';
 import ReactDOM from 'react-dom';
-// import Root from './src/Root';
+import Root from './src/Root';
 
 
 const Index =hot(() => {
     return (
-        // <Root />
-        <h1>This is not happening</h1>
+        <Root />
     );
 });
 
