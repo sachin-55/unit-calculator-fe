f8f41f6ce53cdcfc97c2652c79985cac24c98043
sachin <sachinbhattarai55@gmail.com>
2020-05-08 16:00:16 +0545

    dot env configured for accessing env file

--- webpack.config.js
+++ webpack.config.js
@@ -1,92 +1,95 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path')
 const Fiber = require('fibers');
-const webpack = require("webpack");
-const dotenv = require("dotenv");
+// const webpack = require("webpack");
+const env = require("dotenv").config();
 const CopyPlugin =require("copy-webpack-plugin");
 
-const env = dotenv.config({ path: `${__dirname}/config.env` }).parsed;
+// const env = dotenv.config({ path: `${__dirname}/config.env` }).parsed;
 
-// reduce it to a nice object, the same as before
-const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
+// // reduce it to a nice object, the same as before
+// const envKeys = Object.keys(env).reduce((prev, next) => {
+//     prev[`process.env.${next}`] = JSON.stringify(env[next]);
+//     return prev;
+//   }, {});
   
-module.exports={
-    entry: [
-        './index.js' // Your appʼs entry point
-    ],
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index_bundle.js',
-        publicPath: '/'
-    },
-    module:{
-        rules:[
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+module.exports=(env)=>{
+
+    return({
+            entry: [
+                './index.js' // Your appʼs entry point
+            ],
+            output: {
+                path: path.resolve(__dirname, 'build'),
+                filename: 'index_bundle.js',
+                publicPath: '/'
             },
-            {
-                test: /\.html$/,
-                use: [
+            module:{
+                rules:[
                     {
-                        loader: "html-loader"
-                    }
+                        test: /\.(js|jsx)$/,
+                        exclude: /node_modules/,
+                        use: {
+                            loader: "babel-loader"
+                        }
+                    },
+                    {
+                        test: /\.html$/,
+                        use: [
+                            {
+                                loader: "html-loader"
+                            }
+                        ]
+                    },
+                    {
+                        test: /\.(s[ac]ss|css)$/,
+                        use: [
+                            { loader: 'style-loader' },
+                            {
+                                loader: 'css-loader',
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    implementation: require('dart-sass'),
+                                    sassOptions: {
+                                        fiber: Fiber,
+                                    },
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        test: /\.(png|jpe?g|gif)$/i,
+                        use: [
+                            {
+                                loader: 'file-loader',
+                            },
+                        ],
+                    },
                 ]
             },
-            {
-                test: /\.(s[ac]ss|css)$/,
-                use: [
-                    { loader: 'style-loader' },
-                    {
-                        loader: 'css-loader',
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            implementation: require('dart-sass'),
-                            sassOptions: {
-                                fiber: Fiber,
-                            },
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
-        ]
-    },
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+            mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 
-    plugins:[
-        new HtmlWebPackPlugin({
-            template: "./index.html",
-            filename: "./index.html"
-        }),
-        new CopyPlugin([
-            { from: '_redirects' }
-          ]),
-        new webpack.DefinePlugin(envKeys)
+            plugins:[
+                new HtmlWebPackPlugin({
+                    template: "./index.html",
+                    filename: "./index.html"
+                }),
+                new CopyPlugin([
+                    { from: '_redirects' }
+                ]),
+                // new webpack.DefinePlugin(envKeys)
 
-    ],
-    devServer:{
-        contentBase:'./build',
-        host:'localhost',
-        port:process.env.PORT,
-        historyApiFallback:true,
-        open:true,
-        hot:true,
-        inline:true
-    }
+            ],
+            devServer:{
+                contentBase:'./build',
+                host:'localhost',
+                port:process.env.PORT,
+                historyApiFallback:true,
+                open:true,
+                hot:true,
+                inline:true
+            }
+});
 }
