b7e5a7786e05ab6e97393a9985046a9fbe6a99fd
sachin <sachinbhattarai55@gmail.com>
2020-05-15 15:46:25 +0545

    unit calculation

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Home from './Home';
 
 const Dashboard = () => {
     return (
         <>
           <Navbar/>  
+          <Home/>
         </>
     );
 }
